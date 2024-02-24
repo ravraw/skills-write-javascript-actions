3b7f03b701f5b71c73a6d445a285c6a9d09ea378
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-02-24 16:50:26 -0700

    Corrected setOutput

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,10 +1,10 @@
 const getJoke = require("./joke");
-const setOutput = require("@actions/core");
+const core = require("@actions/core");
 
 async function run() {
   const joke = await getJoke();
   console.log(joke);
-  setOutput("joke-output", joke);
+  core.setOutput("joke-output", joke);
 }
 
 run();
